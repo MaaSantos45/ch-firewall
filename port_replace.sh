86a38925dec73790ecac3dd5097328777c81193c
MaaSantos45 <maasantos_2018@outlook.com>
2024-10-24 11:08:11 -0300

    Update port_replace.sh

--- port_replace.sh
+++ port_replace.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+usage() {
+        echo "Usage: $0 <ips_file>"
+        echo
+        echo "this script remove ':None' from ips_file"
+        echo
+}
+
+
 if [[ "$#" -lt 1 ]];then
         echo "need to provide input file"
+        usage
         exit 1;
 fi
 
@@ -11,6 +20,11 @@ for ip in $(cat "$1");do
         fi
 done
 
-cat ips_noport.txt | sort -u > blacklist.txt
+cat ips_noport.txt | sort -u >> blacklist.txt
+cat blacklist.txt | sort -u > nblk.txt
+
 rm ips_noport.txt
+rm blacklist.txt
 rm "$1"
+
+mv nblk.txt blacklist.txt
